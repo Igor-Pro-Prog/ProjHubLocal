a40ff420f152d1c344dfd164d0e52c1c1e1eba57
igorUfca <97804247+igorUfca@users.noreply.github.com>
2022-01-18 17:54:29 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 export function App() {
   return (
 
-   <main classNome="contentWrapper">
+   <main className="contentWrapper">
      <h1>A Hublocal é uma grande empresa, com ela você consegue informações sobre o seu negócio sempre completas, atualizadas e não perca mais oportunidade para sua concorrência local. Com as funcionalidades de divulgação do Hublocal, divulgue facilmente promoções, cupons de descontos e campanhas regionais de sua empresa sem complicações. E tudo nos sites, apps e diretórios que mais interessam. Dessa forma, faça seu negócio crescer com a Hublocal.  </h1>
    
    </main>
